06c82e57a3ffb7f59f85825bb259d1320aeac50c
Drosophila <Drosophila@DESKTOP-NNJTLS3>
2020-03-14 14:54:30 +0300

    Add project files.

---
+++ ProbablityOfWhiteBall/Helpers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ProbablityOfWhiteBall
+{
+    public static class Helpers
+    {
+        public static List<List<int>> CreateBag()  //All bags are created. 
+        {
+            List<List<int>> allBags = new List<List<int>>();
+
+            for (int i = 2; i < 102; i++)
+            {
+                List<int> singleBag = new List<int>();
+                int counter = i;
+                while (counter > 0)
+                {
+                    singleBag.Add(0);
+                    singleBag.Add(1);
+                    counter--;
+                }
+                Shuffle(singleBag);
+                allBags.Add(singleBag);
+            }
+            return allBags;
+        }
+
+        public static void Shuffle<T>(this IList<T> list)  //The balls in the bag is mixed..
+        {
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            int n = list.Count;
+            while (n > 1)
+            {
+                byte[] box = new byte[1];
+                do provider.GetBytes(box);
+                while (!(box[0] < n * (Byte.MaxValue / n)));
+                int k = (box[0] % n);
+                n--;
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
---
+++ ProbablityOfWhiteBall/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ProbablityOfWhiteBall
+{
+    class Program
+    {
+        static void Main(string[] args)   //In this project, 0 is accepted for black balls and 1 for white balls.
+        {
+            float allConditions = 1000; //Number of experiments carried out
+            float whiteConditionsCounter = 0;  //Counter that counts when both cases A and B (A: first ball is white and B: last ball is white) are white. 
+            
+            for (int t = 0; t < allConditions; t++)   //The experiment is repeated as much as the value of variable allConditions (For now, 100k times).
+            {
+                var Bags = Helpers.CreateBag();    //Each time the experiment is repeated, the bags are created again with the helper method.
+                int firstBall = 0;
+                for (int i = 0; i < Bags.Count; i++)
+                {
+                    for (int j = 0; j < Bags.Count - 1; j++)   //The ball is selected from each bucket and thrown to the next.
+                    {
+                        var random = new Random();
+                        int index = random.Next(Bags[j].Count);
+                        int temp = Convert.ToInt32(Bags[j][index]);
+                        Bags[j].RemoveAt(index);
+                        Bags[j + 1].Add(temp);
+                        if (j == 0 && temp == 1)
+                        {
+                            firstBall = 1;    //The first ball is checked to be white.
+                        }
+                    }
+
+                    for (int k = Bags.Count - 1; k > 0; k--)   //The balls are thrown back to the next bag from 100.
+                    {
+                        var random = new Random();
+                        int index = random.Next(Bags[k].Count);
+                        int temp = Convert.ToInt32(Bags[k][index]);
+                        Bags[k].RemoveAt(index);
+                        Bags[k - 1].Add(temp);
+                    }
+                }
+                var randomSelect = new Random();
+                int lastIndex = randomSelect.Next(Bags[0].Count);
+                int lastBall = Convert.ToInt32(Bags[0][lastIndex]);   //A random ball is selected from the first bag.
+
+                if (lastBall == 1 && firstBall == 1)
+                {
+                    whiteConditionsCounter++;     //The first and last ball white cases are counted here.
+                }
+                Console.WriteLine("step: {0}", t + 1);
+            }
+            float probablity = whiteConditionsCounter / allConditions;      //Probability is calculated by proportioning the cases where the first and last ball is white to the whole case..
+            Console.WriteLine("*******************************Last probablity = {0} *********************************", probablity);
+            Console.ReadKey();
+        }
+    }
+}
